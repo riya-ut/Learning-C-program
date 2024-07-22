88ba81104208287149bc1d170a6552e6cc423d0c
Riya UT <156648774+riya-ut@users.noreply.github.com>
2024-07-22 14:24:39 +0530

    Add files via upload

---
+++ userInputPizzaSms.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+char name[30];
+char size;
+int personItServes;
+float retailPrice;
+
+printf ("Enter pizza name:");
+scanf ("%[^\n]%*c", name);
+
+printf("Enter the size of pizza as either 's', 'M' or 'L'");
+scanf("%c", &size);
+
+printf("Enter the number of person it serves");
+scanf ("%d", &personItServes);
+
+printf("Enter the retail price");
+scanf ("%f", &retailPrice);
+
+printf ("\nGenerating message....\n");
+usleep (1000000);
+
+printf("\nYay! You have ordered a %s pizza of size %c that serves %d.\nPlease "
+   "pay Rs. %.1f to the delivery person.", name, size, personItServes, retailPrice);
+
+printf ("\n\nSending SMS...\n");
+usleep(1000000);
+
+printf("\n\n********SMS SENT********\n\n");
+        return 0;
+        }
